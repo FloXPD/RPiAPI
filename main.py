32f93e25dd675a6f3832e840d4d7422ede1fa8a7
Wlodek <gebicki.wlodek@gmail.com>
2022-10-18 09:23:31 +0200

    test

--- main.py
+++ main.py
@@ -1,50 +1,29 @@
-# Raspberry Pi API for a 8x1 LED Matrix
+#python script to control a 8 pixel led strip raspberry pi with BCM GPIO
 
-import time
+# Import modules
 import RPi.GPIO as GPIO
-import sys
+import time
+import random
 
-# Pin Definitions
-output_pin = 10  # BOARD pin 11, BCM pin 17
+# Set the GPIO naming convention
+GPIO.setmode(GPIO.BCM)
 
-# Pin Setup:
-GPIO.setmode(GPIO.BCM)  # Broadcom pin-numbering scheme
-GPIO.setup(output_pin, GPIO.OUT)  # LED pin set as output
+# Define GPIO pins
+GPIO.setup(10, GPIO.OUT)
 
-# Initial state for LEDs:
-GPIO.output(output_pin, GPIO.HIGH)
+# Define functions
+def red():
+    GPIO.output(18, GPIO.HIGH)
 
+def main():
+    while True:
+        red()
 
-# Helper functions
-def blink(pin):
-    GPIO.output(pin, GPIO.LOW)
-    time.sleep(0.5)
-    GPIO.output(pin, GPIO.HIGH)
-    time.sleep(0.5)
-
-
-def cleanup():
-    print("Cleaning up!")
-    GPIO.cleanup()  # cleanup all GPIO
-
-
-# Main loop
 if __name__ == '__main__':
-    try:
-        print("Starting main loop")
-        while True:
-            blink(output_pin)
-    except KeyboardInterrupt:  # If CTRL+C is pressed, exit cleanly:
-        cleanup()
-    except Exception as e:
-        print(e)
-        cleanup()
-        sys.exit(1)
+    main()
 
-# If you are new to Raspberry Pi GPIO, you can find more information here.
-#
-# I have also created a similar tutorial on how to setup a Raspberry Pi GPIO with a 8x8 LED Matrix.
-#
-# Have fun!
-#
-# Related
+# End of script
+
+# Run the script
+# chmod +x main.py
+# ./main.py
